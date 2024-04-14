c961f46939fe115f1e0f1a0c4bedeffe6e8d2a87
Vikram <vikram.gupta.2513@gmail.com>
2024-04-15 01:56:56 +0530

    set config of deploy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Cloud-Point/",
+  server: {
+    host: "0.0.0.0",
+  },
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: "./src/main.jsx",
+      },
+    },
+  },
 });
